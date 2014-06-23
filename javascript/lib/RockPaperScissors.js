a397b0cf0791f3628277a1fdfd8fe2fe5d939e5b
Painted <will.allen@paintedltd.co.uk>
2014-06-23 14:22:52 +0100

    made rock paper scissors work with js

--- javascript/lib/RockPaperScissors.js
+++ javascript/lib/RockPaperScissors.js
@@ -2,8 +2,16 @@ function Player(name) {
   this.name = name;
 };
 
+var rock, paper, scissors, beats, drawGameResults;
+
 Player.prototype.picks = function(pick) {
   this.pick = pick;
+};
+
+var pairs = {
+	"rock": {"beats": "scissors"},
+	"paper": {"beats": "rock"},
+	"scissors": {"beats": "paper"}
 };
 
 function Game(player1, player2) {
@@ -12,7 +20,15 @@ function Game(player1, player2) {
 };
 
 Game.prototype.winner = function() {
-  if(1 === 1) {
+	if (this.player1.pick === this.player2.pick) {
+		return null
+	}	
+  else if (pairs[this.player1.pick].beats === this.player2.pick) {
     return this.player1;
   }
+  else {return this.player2;}
 };
+
+// Game.prototype.samePick = function() {
+// 	this.player1.pick === this.player2.pick
+// };
